feacd6c68706176accb0190bd18143f6f977236f
João Pedro Miranda Salim <joaopedromirandasalim@gmail.com>
2023-11-22 20:26:28 -0300

    Comenta no main.js

---
+++ src/main.js
@@ -0,0 +1,2 @@
+//Não estava presente na última aula e não pude ver o que foi feito no computador na antepassada. 
+//Por isso, meu main.js não tem nada. Posso corrigir isso se souber como o main.js estava.
